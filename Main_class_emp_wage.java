729ba0c3d54f45ea3518443d88a3853c6f8cc5bb
subramanyam <sbalasubramanyam98@gmail.com>
2022-09-02 12:34:30 +0530

    added: printing cards method

--- Main_class_emp_wage.java
+++
@@ -1,8 +0,0 @@
-// main class for UC-7
-
-class Main_class_emp_wage {
-   public static void main(String[] args) {
-      EmpWageProblem obj = new EmpWageProblem();
-      obj.computation_of_emp_wage();
-   }
-}
